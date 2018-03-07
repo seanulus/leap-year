35a01f39eee37b4ae71ce5539f6be61b741f3110
Sean McDemrott <seanjmac@hotmail.com>
2018-03-07 15:34:48 -0800

    add third spec passing

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
     event.preventDefault();
 
     var leapYear = function(year) {
-      if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        return false;
+      } else if (year % 4 === 0) {
         return true;
       } else {
       return false;
