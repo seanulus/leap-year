f443efc968b71c78a8e270d06f204ef0e9ffdb4a
Sean McDemrott <seanjmac@hotmail.com>
2018-03-07 15:53:51 -0800

    add UI logic displaying result in a sentence for the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
+//BIZ Logic
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+  return false;
+  }
+};
+//UI Logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
 
-    var leapYear = function(year) {
-      if ((year % 4 === 0) && (year % 100 !== 0)) {
-        return true;
-      } else {
-      return false;
-      }
-    };
-
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if(!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
 
   });
 });
